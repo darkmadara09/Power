7c5ee3a565821e4a3093a760b795550050c06eb2
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-07-06 16:40:42 +0530

    Update helper.py

--- Uchiha/helper.py
+++ Uchiha/helper.py
@@ -1,16 +1,16 @@
 HELP_IMG = [
-    "https://telegra.ph/file/e6f09f69413c28779dadb.jpg"
-    "https://telegra.ph/file/7fdf67c75568d15169e51.jpg"
+    "https://graph.org/file/9b8b09ce1d5d48f8004f3.jpg"
+    "https://graph.org/file/60e01e9303094d231be1f.jpg"
 ]
 
 START_VIDEO = ["https://graph.org/file/2904688233933fd7e64fc.mp4"]
 
 
 PHOTO = [
-    "https://telegra.ph/file/415572921efa1be62778a.jpg",
-    "https://telegra.ph/file/8abcd068ce548f9e34fe5.jpg",
-    "https://telegra.ph/file/e6f09f69413c28779dadb.jpg"
-    "https://telegra.ph/file/3d0aba218e951e7c85d7d.jpg",
-    "https://telegra.ph/file/3269f45bf50a7dcdf1575.jpg",
-    "https://telegra.ph/file/7fdf67c75568d15169e51.jpg",
+    "https://graph.org/file/9b8b09ce1d5d48f8004f3.jpg",
+    "https://graph.org/file/60e01e9303094d231be1f.jpg",
+    "https://graph.org/file/875f7cb814130c1fd7ca1.jpg"
+    "https://graph.org/file/5c113ab1fd9690eaf16c8.jpg",
+    "https://graph.org/file/6468a53d4774b0b677ddb.jpg",
+    "https://graph.org/file/9b8b09ce1d5d48f8004f3.jpg",
 ]
