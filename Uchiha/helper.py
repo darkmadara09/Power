055e3de6070047a4f7ea1421945976277bd4879e
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-07-06 09:35:50 +0530

    Add files via upload

---
+++ Uchiha/helper.py
@@ -0,0 +1,16 @@
+HELP_IMG = [
+    "https://telegra.ph/file/e6f09f69413c28779dadb.jpg"
+    "https://telegra.ph/file/7fdf67c75568d15169e51.jpg"
+]
+
+START_VIDEO = ["https://telegra.ph/file/a9f580274ce215e7ad5d5.mp4"]
+
+
+PHOTO = [
+    "https://telegra.ph/file/415572921efa1be62778a.jpg",
+    "https://telegra.ph/file/8abcd068ce548f9e34fe5.jpg",
+    "https://telegra.ph/file/e6f09f69413c28779dadb.jpg"
+    "https://telegra.ph/file/3d0aba218e951e7c85d7d.jpg",
+    "https://telegra.ph/file/3269f45bf50a7dcdf1575.jpg",
+    "https://telegra.ph/file/7fdf67c75568d15169e51.jpg",
+]
