9ba2605a4ffe6bc6cfc189cd0308ae091f1df372
darkmadara09 <136217534+darkmadara09@users.noreply.github.com>
2024-04-15 08:56:09 +0530

    Update config.py

--- Madara/config.py
+++ Madara/config.py
@@ -15,9 +15,9 @@ def get_user_list(config, key):
 class Config(object):
     LOGGER = True
     # ᴀᴅᴅ ʏᴏᴜʀ ᴠᴇʀs  (ᴍᴀɪɴ ᴠᴇʀs)
-    API_ID = int(getenv("API_ID", "16743442"))
-    API_HASH = getenv("API_HASH", "12bbd720f4097ba7713c5e40a11dfd2a")
-    EVENT_LOGS = int(getenv("EVENT_LOGS", "-1001922732593"))
+    API_ID = int(getenv("API_ID", "13279715"))
+    API_HASH = getenv("API_HASH", "56e198053932ecf216af72a2ddffcd78")
+    EVENT_LOGS = int(getenv("EVENT_LOGS", "-1002139608040"))
     DATABASE_URL = getenv(
         "DATABASE_URL",
         "postgres://pjwfzthq:eGF0tslmouCIz1S-FtDs1WGFW0DrIOi8@arjuna.db.elephantsql.com/pjwfzthq",
@@ -25,21 +25,21 @@ class Config(object):
     REDIS_URL = "redis://default:neko69@redis-18084.c289.us-west-1-2.ec2.cloud.redislabs.com:18084/Neko-Free-db"  # redis.os
     MONGO_DB_URL = getenv(
         "MONGO_DB_URL",
-        "mongodb+srv://vortex:yNNrzMsR0BAiI4iY@cluster0.8sizo.mongodb.net/?retryWrites=true&w=majority",
+        "mongodb+srv://Obito:Obito@obito.uolt8k3.mongodb.net/?retryWrites=true&w=majority",
     )
-    TOKEN = getenv("TOKEN", "6330964987:AAGGGM2iz-2hTmZLkiigTQgTCHMPePRC6AY")
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "SexyNano")
-    OWNER_ID = int(getenv("OWNER_ID", "6198858059"))
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "JHBots")
+    TOKEN = getenv("TOKEN", "6494077922:AAEeBBCclIvvhxw9zz_E1cxP96esJzbKZDk")
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "King_Akaza_Shadow")
+    OWNER_ID = int(getenv("OWNER_ID", "7122227582"))
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "Friends_Zone_l")
 
     # ɴᴏᴛ ɪᴍᴘᴏʀᴛᴀɴᴛ ᴢᴏɴᴇ, ɴᴏ ɴᴇᴇᴅ ᴛᴏ ᴇᴅɪᴛ
     MONGO_DB = "Madara"  # ⚠️ ᴅᴏɴ'ᴛ ᴇᴅɪᴛ
     ARQ_API_URL = "arq.hamker.dev"  # ⚠️ ᴅᴏɴ'ᴛ ᴇᴅɪᴛ
-    ARQ_API_KEY = "YZXQNZ-TPCRLZ-HKWWKY-SPPYAL-ARQ"
-    DONATION_LINK = "t.me/SexyNano"
+    ARQ_API_KEY = "GFUXSS-XJINVJ-CMIQKI-NHHBNS-ARQ"
+    DONATION_LINK = "t.me/obito_shadow"
     HELP_IMG = "https://graph.org/file/9b8b09ce1d5d48f8004f3.jpg"
     START_VIDEO = "https://graph.org/file/2904688233933fd7e64fc.mp4"
-    UPDATES_CHANNEL = "JujutsuHighNetwork"
+    UPDATES_CHANNEL = "Ix_Updates"
     INFOPIC = False
     GENIUS_API_TOKEN = (
         "gIgMyTXuwJoY9VCPNwKdb_RUOA_9mCMmRlbrrdODmNvcpslww_2RIbbWOB8YdBW9"
@@ -62,7 +62,7 @@ class Config(object):
     ALLOW_EXCL = []
     ALLOW_CHATS = True
     CERT_PATH = []
-    SPAMWATCH_SUPPORT_CHAT = "Anime_Krew"
+    SPAMWATCH_SUPPORT_CHAT = "Friends_Zone_l"
     BOT_API_URL = "https://api.telegram.org/bot"  # ⚠️ ᴅᴏɴ'ᴛ ᴇᴅɪᴛ
     DRAGONS = get_user_list("elevated_users.json", "sudos")  # ⚠️ ᴅᴏɴ'ᴛ ᴇᴅɪᴛ
     DEV_USERS = get_user_list("elevated_users.json", "devs")  # ⚠️ ᴅᴏɴ'ᴛ ᴇᴅɪᴛ
